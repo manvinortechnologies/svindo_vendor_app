d7418ee8c5de1b580e3d19cd12435dc7d7c3ec56
mahirsodagar <mahirsodagar007@gmail.com>
2026-03-07 16:23:10 +0530

    feat: Integrate Firebase services, update project dependencies, refresh app icons, and add privacy manifest.

--- ios/Svindovender/AppDelegate.swift
+++ ios/Svindovender/AppDelegate.swift
@@ -2,18 +2,28 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import FirebaseCore
+import Firebase
 
 @main
 class AppDelegate: RCTAppDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     self.moduleName = "Svindovender"
     self.dependencyProvider = RCTAppDependencyProvider()
+    FirebaseApp.configure()
 
     // You can add your custom initial props in the dictionary below.
     // They will be passed down to the ViewController used by React Native.
     self.initialProps = [:]
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    if Auth.auth().canHandle(url) {
+      return true
+    }
+    return super.application(app, open: url, options: options)
   }
 
   override func sourceURL(for bridge: RCTBridge) -> URL? {
